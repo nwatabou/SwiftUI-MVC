800f296bc807dce4d0ff59ab23b5442dd05d25d0
nwatabou <wataru1.11@gmail.com>
2022-02-10 08:36:34 +0900

    add Project

---
+++ Source/SwiftUI_MVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVCApp.swift
+//  SwiftUI-MVC
+//
+//  Created by nakanishi wataru on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
